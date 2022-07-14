9145358879f12119a40de1446b05b8825bd91202
Ahrry <gopalahrry@gmail.com>
2022-07-14 21:22:46 +0200

    feat(level2) add posibility to apply discount

--- services/compute_rental_price.rb
+++ services/compute_rental_price.rb
@@ -1,18 +1,18 @@
 require './services/base_service'
-require 'byebug'
+require './services/car_day_price'
 
 class ComputeRentalPrice < BaseService
-  def call
+  def call(with_discount: false)
     result = []
 
     json_rentals.each do |json_rental|
       rental = Rental.new(json: json_rental)
       car = Car.new(json: json_car(id: rental.car_id))
 
-      result << {
-        id: rental.id,
-        price: day_price(car: car, rental: rental) + km_price(car: car, rental: rental)
-      }
+      price = day_price(car: car, rental: rental, with_discount: with_discount).to_i +
+              km_price(car: car, rental: rental).to_i
+
+      result << { id: rental.id, price: price }
     end
 
     { rentals: result }.to_json
@@ -20,8 +20,9 @@ class ComputeRentalPrice < BaseService
 
   private
 
-  def day_price(car:, rental:)
-    rental.length * car.price_per_day
+  def day_price(car:, rental:, with_discount:)
+    card_day_price = CarDayPrice.new(price: car.price_per_day, rental_length: rental.length)
+    with_discount ? card_day_price.price_with_discount : card_day_price.price_without_discount
   end
 
   def km_price(car:, rental:)
