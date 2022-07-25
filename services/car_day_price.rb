9d8e424bea137d64c20b0c869cd4d2547fc89218
Ahrry <gopalahrry@gmail.com>
2022-07-25 19:36:54 +0200

    refacto(car_day_price) about discount

--- services/car_day_price.rb
+++ services/car_day_price.rb
@@ -1,10 +1,11 @@
 require 'byebug'
 
-DISCOUNT_FACTORS = {
-  after_ten_days: { nb_days: 10, factor: 0.5 },
-  after_four_days: { nb_days: 4, factor: 0.3 },
-  after_one_day: { nb_days: 1, factor: 0.1 }
-}.freeze
+DISCOUNT_FACTORS = [
+  { after_nb_day: 10, factor: 0.5 },
+  { after_nb_day: 4, factor: 0.3 },
+  { after_nb_day: 1, factor: 0.1 },
+  { after_nb_day: 0, factor: 0 }
+].freeze
 
 class CarDayPrice
   def initialize(price:, rental_length:)
@@ -16,46 +17,25 @@ class CarDayPrice
     @rental_length * @price
   end
 
-  # rubocop:disable Metrics/AbcSize
   def price_with_discount
-    [
-      nb_days_with_fifty_percent_discount * @price * (1 - DISCOUNT_FACTORS[:after_ten_days][:factor]),
-      nb_days_with_thirty_percent_discount * @price * (1 - DISCOUNT_FACTORS[:after_four_days][:factor]),
-      nb_days_with_ten_percent_discount * @price * (1 - DISCOUNT_FACTORS[:after_one_day][:factor]),
-      nb_days_without_discount * @price
-    ].sum
+    rental_length = @rental_length
+
+    after_nb_days.map do |nb_day|
+      remaining_days = rental_length - nb_day
+      nb_days_with_discount = remaining_days.negative? ? 0 : remaining_days
+      rental_length -= nb_days_with_discount
+
+      nb_days_with_discount * @price * (1 - discount_factor(nb_day: nb_day))
+    end.sum
   end
-  # rubocop:enable Metrics/AbcSize
 
   private
 
-  def nb_days_with_fifty_percent_discount
-    nb_days = @rental_length - DISCOUNT_FACTORS[:after_ten_days][:nb_days]
-
-    nb_days.negative? ? 0 : nb_days
+  def discount_factor(nb_day:)
+    DISCOUNT_FACTORS.detect { |discount_factor| discount_factor[:after_nb_day] == nb_day }[:factor]
   end
 
-  def nb_days_with_thirty_percent_discount
-    nb_days = @rental_length -
-              nb_days_with_fifty_percent_discount -
-              DISCOUNT_FACTORS[:after_four_days][:nb_days]
-
-    nb_days.negative? ? 0 : nb_days
-  end
-
-  def nb_days_with_ten_percent_discount
-    nb_days = @rental_length -
-              nb_days_with_fifty_percent_discount -
-              nb_days_with_thirty_percent_discount -
-              DISCOUNT_FACTORS[:after_one_day][:nb_days]
-
-    nb_days.negative? ? 0 : nb_days
-  end
-
-  def nb_days_without_discount
-    @rental_length -
-      nb_days_with_fifty_percent_discount -
-      nb_days_with_thirty_percent_discount -
-      nb_days_with_ten_percent_discount
+  def after_nb_days
+    @after_nb_days ||= DISCOUNT_FACTORS.map { |discount_factor| discount_factor[:after_nb_day] }.sort { |a, b| b <=> a }
   end
 end
